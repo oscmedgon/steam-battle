7e72259bcb55d7c8eebf085f4967d47a36283895
oscmedgon <oscmedgon@gmail.com>
2017-10-07 18:36:57 +0200

    user needs to introduce data before magics

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,12 @@
 $('#user-input').on('submit', function(event){
   event.preventDefault()
-  var player1 = $('#player1').val()
-  var player2 = $('#player2').val()
-  var player2 = $('#player2').val()
+  var players = [$('#player1').val()]
+  if($('#player2').val()){
+    players.push($('#player2').val())
+  }
+  if($('#player3').val()){
+    players.push($('#player3').val())
+  }
+
+  console.log( `${players} ${players.length}`)
 })
