76f9029e5d01cf487edf580df61de14be18b5477
oscmedgon <oscmedgon@gmail.com>
2017-10-07 20:26:02 +0200

    i'm going crazy

---
+++ scripts/services/ServiceSteam.js
@@ -0,0 +1,30 @@
+(function() {
+
+  const token = 'A463AB6CD2DB0B7A0CB98FD7270A3D43'
+
+  const vanityNameSearch = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/'
+  const urlGetAlbums = 'https://api.spotify.com/v1/artists/<%ID_ARTIST%>/albums'
+  const urlGetTracks = 'https://api.spotify.com/v1/albums/<%ID_ALBUM%>/tracks'
+
+  // function searchSteamID( vanityName ) {
+  //   var urlVanity = urlGetAlbums.replace('<%VANITI_NAME%>', vanityName).replace('<%TOKEN%>' , token)
+  //   const url = urlVanity
+  //
+  //   return $.ajax({
+  //     url: urlVanity
+  //   })
+  // }
+  function searchSteamID(vanityName) {
+    var urlVanity = urlGetAlbums.replace('<%VANITI_NAME%>', vanityName).replace('<%TOKEN%>' , token)
+
+    return ($.ajax({
+      url: vanityNameSearch,
+      data: {
+        vanityurl : vanityName,
+        key : token,
+      }
+    })
+  )}
+  window.STEAM_SERVICE = {searchSteamID}
+
+})()
