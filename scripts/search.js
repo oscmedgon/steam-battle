292f117465d1c609891c2ddc08c67560df7c640f
oscmedgon <oscmedgon@gmail.com>
2017-10-07 18:10:37 +0200

    adding some logic

---
+++ scripts/search.js
@@ -0,0 +1,6 @@
+$('#user-input').on('submit', function(event){
+  event.preventDefault()
+  var player1 = $('#player1').val()
+  var player2 = $('#player2').val()
+  var player2 = $('#player2').val()
+})
