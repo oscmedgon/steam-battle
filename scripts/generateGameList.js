3cfe83f5e28fdb84809937c2101edd72a336ac09
waken22 <waken22jrb@gmail.com>
2017-10-08 17:14:57 +0200

    More changes in generateGameList

--- scripts/generateGameList.js
+++ scripts/generateGameList.js
@@ -1,13 +1,41 @@
-// THIS FUNCTION SHOULD BE CALLED WITH AN ARRAY WITH TWO OR THREE STRINGS INSIDE
-function generateGameList(players){
-  players.forEach(function(player){
-    console.log(player)
-    STEAM_SERVICE.searchUserGameList(player)
-    .then(function(oData){
-    const gameListCount = oData.response.game_count
-    const gameList = oData.response.games
-    gameList.forEach(function(list){userList.push(list.appid)})
+/* global STEAM_SERVICE   */
+// THIS FUNCTION SHOULD BE CALLED WITH AN ARRAY WITH ONE OR THREE STRINGS INSIDE
+var testArray = [ '76561198000981199', '76561198017534701', '76561198000981199' ]
+function generateGameList (players) {
+  var iWillGetMyArray = new Promise(
+    function (resolve, reject) {
+      var gameList = []
+      players.forEach(function (player, index) {
+        STEAM_SERVICE.searchUserGameList(player)
+        .then(function (oData) {
+          console.log('He iterado una vez!!')
+          const gameListCount = oData.response.game_count // eslint-disable-line 
+          gameList.push(oData.response.games)
+          console.log(gameList[index])
+          console.log(index)
+          console.log(players.length + ' ' + gameList.length)
+          if (players.length === gameList.length) {
+            resolve(gameList)
+          }
+        })
+      })
+    }
+  )
+  iWillGetMyArray.then(function (resolve) {
+    var idSorted = resolve.sort(function (a, b) {
+      return a.length - b.length
     })
-    console.log(userList)
+    var filteredGameList = idSorted[0].filter(function (idEle) {
+      let newId = idEle
+      if (idSorted.length === 2) {
+        if (idSorted[1].find(function (id) { return id = newId }) === true) {
+          return 
+        }
+      }
     })
-  }
+    console.log(`This is the gameList : ${filteredGameList}`)
+    console.log(idSorted[0])
+  })
+}
+
+generateGameList(testArray)
